8181ed5ac379f35a0906b2d463baf335ad47e9c6
lensatesema <lensatesema19@gmail.com>
2023-02-20 10:44:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print a line of code using puts
+ * main - Entry point
  *
- * Return: 0
-*/
-
-int main(viod)
-
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
+
