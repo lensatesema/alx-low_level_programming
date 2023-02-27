f3bb329687c995ec554b1f456d319d0319ade10f
lensatesema <lensatesema19@gmail.com>
2023-02-27 10:37:47 -0800

    0-reset_to_98.c
    git commit -m 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *rest_to-98 -function that takes a pointer to an int as parameter and
+ *updates the value it ponts to 98
+ *
+ * @n: ths is thr entry
+ *
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
