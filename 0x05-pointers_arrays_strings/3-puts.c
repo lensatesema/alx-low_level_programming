6f2872a26aae076079a54fbf293a1b5633ad67e2
lensatesema <lensatesema19@gmail.com>
2023-02-27 11:25:05 -0800

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,11 +10,12 @@
 
 void _puts(char *str)
 {
-	int index;
+	int i = 0;
 
-	for (index = 0; str[index] != '\0'; index++)
+	while (str[i])
 	{
-		_putchar(str[index]);
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,20 +4,20 @@
  * puts2 - Write a function that prints every other character of a string, 
  *         starting with the first character, followed by a new line.
  *
- * @s: This pointer point to a char or string
+ * @str: This pointer point to a char or string
  *
  *
  */
 
-void puts2(char *s)
+void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (a % 2 == 0)
 		{
-			_putchar(s[a]);
+			_putchar(str[a]);
 		}
 	}
 	_putchar('\n');
